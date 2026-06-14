0e38ecc83c33c9c4434bcd7ede9c3fdad542fd07
MamathaCoder <mamathawork123@gmail.com>
2026-06-14 17:46:05 +0530

    Commit-Superkeyworddemo

---
+++ Core_Java/src/com/tnsif/superkeyword/Payment1.java
@@ -0,0 +1,19 @@
+package com.tnsif.superkeyword;
+// demo for super method
+class Payment1{
+        void process() {
+                System.out.println("processing payment using standard gateway..");
+        }
+}
+class Gpay extends Payment1{
+        void process() {
+                System.out.println("processing payment using standard gpay..");
+        }
+        
+        void completetransation() {
+                super.process();//parent class
+                process();// child class
+        }
+}
+
+
