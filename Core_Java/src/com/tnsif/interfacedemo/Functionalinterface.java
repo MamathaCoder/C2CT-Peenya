678202e31c90beef2f99f140052b2c017db80e08
Mamathacoder <mamathwork123@gmail.com>
2026-06-29 02:02:42 +0530

    commit interfacetypes

--- Core_Java/src/com/tnsif/interfacedemo/Functionalinterface.java
+++ Core_Java/src/com/tnsif/interfacedemo/Functionalinterface.java
@@ -1,5 +1,43 @@
 package com.tnsif.interfacedemo;
+//demo for functional interface
+
+@FunctionalInterface
+
+interface Notification{
+	void notifyUser(String message);
+	
+}
+//Email notification class 
+class Emailnotification implements Notification{
+
+	@Override
+	public void notifyUser(String message) {
+		// TODO Auto-generated method stub
+		System.out.println("Sending email"+message);
+	}
+	
+}
+//sms notification user
+
+class SMSNotification implements Notification{
+
+	@Override
+	public void notifyUser(String message) {
+		// TODO Auto-generated method stub
+		System.out.println("Sending email"+message);
+	}
+	
+}
 
 public class Functionalinterface {
+public static void main(String[] args) {
+	Notification n=new Emailnotification();
+	
+	Notification s=new SMSNotification();
+	
+	n.notifyUser("assigment upload");
+	s.notifyUser("class at 10 am");
+	
+}
 
 }
