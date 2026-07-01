7a4079e5703bc85ee2492e28a0c819c205b900e0
Mamathacoder <mamathwork123@gmail.com>
2026-07-02 00:59:34 +0530

    Commit Multithreading

--- Core_Java/src/com/tnsif/exceptionhandling/Throwsdemo1.java
+++ Core_Java/src/com/tnsif/exceptionhandling/Throwsdemo1.java
@@ -1,5 +1,26 @@
 package com.tnsif.exceptionhandling;
 
-public class Throwsdemo1 {
+import java.io.FileReader;
+import java.io.IOException;
 
+// demo for throws keyword
+public class Throwsdemo1 {
+static void test() throws IOException,ClassNotFoundException{
+        FileReader file=new FileReader("abc.txt");//may throw io exception
+        Class.forName("Test");
+        
+        file.close();
 }
+public static void main(String[] args) {
+        try {
+                Throwsdemo1.test();
+        }
+        catch(IOException e) {
+                System.out.println("file error");
+        }
+        catch(ClassNotFoundException f) {
+                System.out.println("class not found");
+        }
+}
+}
+
